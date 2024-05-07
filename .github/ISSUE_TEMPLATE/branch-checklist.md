f983d917f00f5a2f382050425c5ad7300bdfd3ae
projectoperations <34292598+projectoperations@users.noreply.github.com>
2024-05-08 00:16:33 +0100

    Initialize dev17.8.x

---
+++ .github/ISSUE_TEMPLATE/branch-checklist.md
@@ -0,0 +1,30 @@
+---
+name: Branch checklist
+about: An outline of the steps needed to create a new branch (internal use)
+title: Create [BRANCH NAME] branch checklist
+labels: Area-Infrastructure
+assignees: ''
+
+---
+
+_Descriptions of these steps can be found in the team OneNote._
+
+<!-- Replace all ❓ characters as you work through this. -->
+
+- [ ] Identify base commit for new branch
+  - [ ] According to the [schedule](https://dev.azure.com/devdiv/DevDiv/_wiki/wikis/DevDiv.wiki/10097/Dev17-Release), `main` snapped to `rel/d17.❓` at ❓ PST
+  - [ ] Last [completed Project System insertion PR](https://dev.azure.com/devdiv/DevDiv/_git/VS/pullrequests?_a=completed&assignedTo=6e89082d-fdd2-4442-a310-051df5bdc73c): https://dev.azure.com/devdiv/DevDiv/_git/VS/pullrequest/❓
+  - [ ] Last inserted GitHub PR: https://github.com/dotnet/project-system/pull/❓
+  - [ ] Merge commit of that PR: ❓
+- [ ] Create branch on GitHub at that commit
+  - [ ] https://github.com/dotnet/project-system/tree/dev17.❓.x
+- [ ] For branches not matching `dev*` or `feature/*` (usually we skip these)
+  - [ ] Update the [pull-request.yml](https://github.com/dotnet/project-system/blob/main/eng/pipelines/pull-request.yml) (via `pr`) to support PR builds
+  - [ ] Update the [official.yml](https://github.com/dotnet/project-system/blob/main/eng/pipelines/official.yml) (via `trigger`) to have signed builds the new branch
+- [ ] Update Roslyn Tools [config.xml](https://github.com/dotnet/roslyn-tools/blob/main/src/GitHubCreateMergePRs/config.xml) file to flow branch changes to the latest dev branch
+  - [ ] dotnet/roslyn-tools PR: https://github.com/dotnet/roslyn-tools/pull/❓
+- [ ] Update [version.json](https://github.com/dotnet/project-system/blob/main/version.json) (via `"version"`) to match the version of VS, if needed
+    - [ ] In new branch: https://github.com/dotnet/project-system/blob/dev17.❓.x/version.json
+    - [ ] In `main`: https://github.com/dotnet/project-system/blob/main/version.json
+- [ ] To run manual insertions, use the [DotNet-Project-System pipeline](https://devdiv.visualstudio.com/DevDiv/_build?definitionId=9675&_a=summary). Enter the GitHub branch as _Branch/tag_, the VS branch as _VS Insertion Branch Name_, and check _Create VS Insertion PR_.
+- [ ] Update [MSFTBot milestone tracking](https://aka.ms/fabricbotconfig)
---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Build/PublishItemsOutputGroupProvider.cs
@@ -0,0 +1,53 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this file to you under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using Microsoft.VisualStudio.Threading;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Build
+{
+    [Export(typeof(IOutputGroupProvider))]
+    [AppliesTo(ProjectCapabilities.VisualStudioWellKnownOutputGroups)]
+    [Order(Order.Default)]
+    internal class PublishItemsOutputGroupProvider : IOutputGroupProvider
+    {
+        private const string PublishItemsOutputGroupTargetName = "PublishItemsOutputGroup";
+
+        /// <summary>
+        /// Collection containing a single "publish items" output group.
+        /// This is a singleton instance, shared across all projects/configurations.
+        /// </summary>
+        private static readonly ImmutableHashSet<IOutputGroup> s_outputGroups = ImmutableHashSet.Create<IOutputGroup>(
+            new OutputGroup(
+                name: "PublishItems",
+                targetName: PublishItemsOutputGroupTargetName,
+                displayName: Resources.OutputGroupPublishItemsDisplayName,
+                description: Resources.OutputGroupPublishItemsDescription,
+                items: ImmutableList<KeyValuePair<string, IImmutableDictionary<string, string>>>.Empty,
+                successful: false));
+
+        private readonly AsyncLazy<IImmutableSet<IOutputGroup>> _outputGroups;
+
+        [ImportingConstructor]
+        internal PublishItemsOutputGroupProvider(
+            IProjectAccessor projectAccessor,
+            ConfiguredProject configuredProject,
+            IProjectThreadingService projectThreadingService)
+        {
+            _outputGroups = new AsyncLazy<IImmutableSet<IOutputGroup>>(
+                GetOutputGroupMetadataAsync,
+                projectThreadingService.JoinableTaskFactory);
+
+            async Task<IImmutableSet<IOutputGroup>> GetOutputGroupMetadataAsync()
+            {
+                bool hasPublishItemsTarget = await projectAccessor.OpenProjectForReadAsync(
+                    configuredProject,
+                    project => project.Targets.ContainsKey(PublishItemsOutputGroupTargetName));
+
+                return hasPublishItemsTarget
+                    ? s_outputGroups
+                    : ImmutableHashSet<IOutputGroup>.Empty;
+            }
+        }
+
+        public Task<IImmutableSet<IOutputGroup>> OutputGroups => _outputGroups.GetValueAsync();
+    }
+}
